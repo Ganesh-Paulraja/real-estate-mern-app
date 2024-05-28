11752388354ae946917330c56aa0501f2acb8662
Ganesh-Paulraja <ganeshsgk555@gmail.com>
2024-05-28 17:39:47 +0530

    react template with tailwind created

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='text-red-500 bg-black  border p-5'>
+      <h1 className="bg-yellow-500 border-2 border-red-500 rounded-sm hover:border-green-500" >gg dd test</h1>
+    </div>
+  )
+}
