815b20402aae92d864fe38645297ece1b5f424c4
Ganesh-Paulraja <ganeshsgk555@gmail.com>
2024-05-30 16:28:29 +0530

    singn up page functionality created

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Signin from './pages/Signin'
-import Signout from './pages/Signup'
+import Signup from './pages/Signup'
 import About from './pages/About'
 import Profile from './pages/Profile'
 import Header from './components/Header'
@@ -15,7 +15,7 @@ export default function App() {
      <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/Signin' element={<Signin/>}/>
-      <Route path='/Signup' element={<Signout/>}/>
+      <Route path='/Signup' element={<Signup/>}/>
       <Route path='/About' element={<About/>}/>
       <Route path='/Profile' element={<Profile/>}/>
      </Routes>
