9ce68da7168c3916161bbabd3f40ab6be62972bc
Ganesh-Paulraja <ganeshsgk555@gmail.com>
2024-07-01 15:52:27 +0530

    create listing page ui completed

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import About from './pages/About'
 import Profile from './pages/Profile'
 import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
+import CreateListing from './pages/CreateListing'
 
 export default function App() {
   return (
@@ -22,6 +23,7 @@ export default function App() {
       <Route path='/About' element={<About/>}/>
       <Route  element={<PrivateRoute/>}>
         <Route path='/Profile' element={<Profile/>}/>
+        <Route path='/create-listing' element={<CreateListing/>}/>
       </Route>
      </Routes>
      </BrowserRouter>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,6 +4,7 @@ import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/
 import { app } from '../firebase';
 import { deleteUserFailure, deleteUserSuccess, deleteUserStart, updateUserFailure, updateUserStart, updateUserSuccess, signOutUserStart } from '../redux/user/userSlice';
 import { useDispatch, useSelector } from 'react-redux';
+import {Link} from 'react-router-dom'
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -156,6 +157,7 @@ export default function Profile() {
         <button disabled={loading} className='bg-slate-700 text-white p-3 uppercase hover:opacity-95 disabled:opacity-80'>
           {loading? "Loading..." : 'Update'}
         </button>
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to={"/create-listing"}>Create Listing</Link>
       </form>
       <div className="flex justify-between mt-5">
         <span className='text-red-700 cursor-pointer' onClick={handleDeleteUser}>Delete account</span>
